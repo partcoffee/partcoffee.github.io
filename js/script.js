81022262a08ffcc79e24af19916d43e5d7bf80f0
partcoffee <magnusskare@Magnuss-MacBook-Pro.local>
2014-10-30 16:54:10 +0100

    Updated project

---
+++ js/script.js
@@ -0,0 +1,77 @@
+window.onload = function() {
+
+	// Video
+	var video = document.getElementById("video");
+
+	// Buttons
+	var playButton = document.getElementById("play-pause");
+
+	// Sliders
+	var seekBar = document.getElementById("seek-bar");
+
+
+	// Event listener for the play/pause button
+	playButton.addEventListener("click", function() {
+		if (video.paused == true) {
+			// Play the video
+			video.play();
+
+			// Update the button text to 'Pause'
+			playButton.innerHTML = "Pause";
+		} else {
+			// Pause the video
+			video.pause();
+
+			// Update the button text to 'Play'
+			playButton.innerHTML = "Play";
+		}
+	});
+
+
+
+
+
+
+
+
+	// Change button to 'play' at the end of the video
+	// Exit video on value 100
+	// Hide controls after 4 seconds of inactivity
+	// Show controls on mouseover, do not fade even though inactive
+
+
+
+
+
+	// Event listener for the seek bar
+	seekBar.addEventListener("change", function() {
+		// Calculate the new time
+		var time = video.duration * (seekBar.value / 100);
+
+		// Update the video time
+		video.currentTime = time;
+	});
+
+	
+	// Update the seek bar as the video plays
+	video.addEventListener("timeupdate", function() {
+		// Calculate the slider value
+		var value = (100 / video.duration) * video.currentTime;
+
+		// Update the slider value
+		seekBar.value = value;
+	});
+
+	// Pause the video when the seek handle is being dragged
+	seekBar.addEventListener("mousedown", function() {
+		video.pause();
+		playButton.innerHTML = "Play";
+	});
+
+	// Play the video when the seek handle is dropped
+	seekBar.addEventListener("mouseup", function() {
+		video.play();
+		playButton.innerHTML = "Pause";
+	});
+}
+
