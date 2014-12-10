52d1073ba773dea866b1d40998027d12cf0faa71
partcoffee <magnusskare@Magnuss-MBP.default>
2014-12-10 08:19:43 +0100

    wip project

--- js/main.js
+++ js/main.js
@@ -1,16 +1,107 @@
-jQuery(document).ready(function($){
-	//open interest point description
-	$('.cd-single-point').children('a').on('click', function(){
-		var selectedPoint = $(this).parent('li');
-		if( selectedPoint.hasClass('is-open') ) {
-			selectedPoint.removeClass('is-open').addClass('visited');
-		} else {
-			selectedPoint.addClass('is-open').siblings('.cd-single-point.is-open').removeClass('is-open').addClass('visited');
+var body = document.body;
+var container = document.getElementById("container");
+
+var isTouchscreen;
+var scrollVal = 0;
+	
+var layer0 = document.getElementById("layer-0");
+var layer1 = document.getElementById("layer-1");
+var layer2 = document.getElementById("layer-2");
+var layer3 = document.getElementById("layer-3");
+
+function unloadTrailer()
+{
+	window.scrollTo(0, 0);
+	
+	body.setAttribute('class', 'noTrailer');
+	
+	while (blackout.firstChild)
+	{
+		blackout.removeChild(blackout.firstChild);
+	}
+		
+}
+
+function setup()
+{
+	var platform = navigator.platform.toLowerCase();
+	var userAgent = navigator.userAgent.toLowerCase();
+	
+	if ( platform.indexOf('ipad') != -1  ||  platform.indexOf('iphone') != -1 ) 
+	{
+		isTouchscreen = 1;
+		container.setAttribute("class", "touch");
+		body.style.backgroundImage = 'none';
+	}
+	
+	else if (platform.indexOf('win32') != -1 || platform.indexOf('linux') != -1)
+	{
+		isTouchscreen = 0;
+						
+		if ($.browser.webkit)
+		{
+			$.srSmoothscroll();
 		}
-	});
-	//close interest point description
-	$('.cd-close-info').on('click', function(event){
-		event.preventDefault();
-		$(this).parents('.cd-single-point').eq(0).removeClass('is-open').addClass('visited');
-	});
-});
+	}
+	
+	else
+	{
+		isTouchscreen = 0;
+	}
+
+}
+
+function parallaxy()
+{
+	offset4 = -(scrollVal * -0.04);
+	offset3 = 0;
+	offset2 = -(scrollVal * -0.12);
+	offset1 = -(scrollVal * -0.2);
+	offset0 = -(scrollVal * -0.26);
+
+	layer0.style.webkitTransform = "translate3d(0, " + offset0 + "px, 0)";
+	layer0.style.MozTransform = "translate3d(0, " + offset0 + "px, 0)";
+	layer0.style.msTransform = "translateY(" + offset0 + "px)";
+	layer0.style.OTransform = "translate3d(0, " + offset0 + "px, 0)";
+	layer0.style.transform = "translate3d(0, " + offset0 + "px, 0)";
+
+	layer1.style.webkitTransform = "translate3d(0, " + offset1 + "px, 0)";
+	layer1.style.MozTransform = "translate3d(0, " + offset1 + "px, 0)";
+	layer1.style.msTransform = "translateY(" + offset1 + "px)";
+	layer1.style.OTransform = "translate3d(0, " + offset1 + "px, 0)";
+	layer1.style.transform = "translate3d(0, " + offset1 + "px, 0)";
+
+	layer2.style.webkitTransform = "translate3d(0, " + offset2 + "px, 0)";
+	layer2.style.MozTransform = "translate3d(0, " + offset2 + "px, 0)";
+	layer2.style.msTransform = "translateY(" + offset2 + "px)";
+	layer2.style.OTransform = "translate3d(0, " + offset2 + "px, 0)";
+	layer2.style.transform = "translate3d(0, " + offset2 + "px, 0)";
+
+	layer3.style.webkitTransform = "translate3d(0, " + offset3 + "px, 0)";
+	layer3.style.MozTransform = "translate3d(0, " + offset3 + "px, 0)";
+	layer3.style.msTransform = "translateY(" + offset3 + "px)";
+	layer3.style.OTransform = "translate3d(0, " + offset3 + "px, 0)";
+	layer3.style.transform = "translate3d(0, " + offset3 + "px, 0)";
+}
+
+
+function scrollHandler(e)
+{
+	scrollVal = Math.max(window.pageYOffset,0);
+	
+	/* UNCOMMENT THESE LINES TO ABORT PARALLAX STUFF */
+/*
+	isTouchscreen = 1;
+	container.setAttribute("class", "touch");
+	body.style.backgroundImage = 'none';
+	firewatchlogo.style.position = 'absolute';
+*/
+	
+	if ( isTouchscreen != 1 )
+	{
+		parallaxy()
+	}
+}
+
+body.onload = setup();
+document.addEventListener('scroll', scrollHandler, false);
