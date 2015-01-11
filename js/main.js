8faa34a97e32a635427578e4af566e1915f8429c
partcoffee <block email>
2015-01-12 00:48:39 +0100

    temporary project

---
+++ js/main.js
@@ -0,0 +1,91 @@
+var body = document.body;
+var top = document.getElementById("top");
+
+var isTouchscreen;
+var scrollVal = 0;
+	
+var layer0 = document.getElementById("layer-0");
+var layer1 = document.getElementById("layer-1");
+var layer2 = document.getElementById("layer-2");
+var layer3 = document.getElementById("layer-3");
+var layer4 = document.getElementById("layer-4");
+
+function setup()
+{
+	var platform = navigator.platform.toLowerCase();
+	var userAgent = navigator.userAgent.toLowerCase();
+	
+	if ( platform.indexOf('ipad') != -1  ||  platform.indexOf('iphone') != -1 ) 
+	{
+		isTouchscreen = 1;
+		top.setAttribute("class", "touch");
+		body.style.backgroundImage = 'none';
+	}
+	
+	else
+	{
+		isTouchscreen = 0;
+	}
+
+}
+
+function parallaxy()
+{
+	offset4 = -(scrollVal * -0.6).toFixed(1);
+	offset3 = -(scrollVal * -0.5).toFixed(1);
+	offset2 = -(scrollVal * -0.4).toFixed(1);
+	offset1 = -(scrollVal * -0.3).toFixed(1);
+	offset0 = 0;
+
+
+	layer0.style.webkitTransform = "translate3d(0, " + offset0 + "px, 0)";
+	layer0.style.MozTransform = "translate3d(0, " + offset0 + "px, 0)";
+	layer0.style.msTransform = "translateY(" + offset0 + "px)";
+	layer0.style.OTransform = "translate3d(0, " + offset0 + "px, 0)";
+	layer0.style.transform = "translate3d(0, " + offset0 + "px, 0)";
+
+	layer1.style.webkitTransform = "translate3d(0, " + offset1 + "px, 0)";
+	layer1.style.MozTransform = "translate3d(0, " + offset1 + "px, 0)";
+	layer1.style.msTransform = "translateY(" + offset1 + "px)";
+	layer1.style.OTransform = "translate3d(0, " + offset1 + "px, 0)";
+	layer1.style.transform = "translate3d(0, " + offset1 + "px, 0)";
+
+	layer2.style.webkitTransform = "translate3d(0, " + offset2 + "px, 0)";
+	layer2.style.MozTransform = "translate3d(0, " + offset2 + "px, 0)";
+	layer2.style.msTransform = "translateY(" + offset2 + "px)";
+	layer2.style.OTransform = "translate3d(0, " + offset2 + "px, 0)";
+	layer2.style.transform = "translate3d(0, " + offset2 + "px, 0)";
+
+	layer3.style.webkitTransform = "translate3d(0, " + offset3 + "px, 0)";
+	layer3.style.MozTransform = "translate3d(0, " + offset3 + "px, 0)";
+	layer3.style.msTransform = "translateY(" + offset3 + "px)";
+	layer3.style.OTransform = "translate3d(0, " + offset3 + "px, 0)";
+	layer3.style.transform = "translate3d(0, " + offset3 + "px, 0)";
+
+	layer4.style.webkitTransform = "translate3d(0, " + offset4 + "px, 0)";
+	layer4.style.MozTransform = "translate3d(0, " + offset4 + "px, 0)";
+	layer4.style.msTransform = "translateY(" + offset4 + "px)";
+	layer4.style.OTransform = "translate3d(0, " + offset4 + "px, 0)";
+	layer4.style.transform = "translate3d(0, " + offset4 + "px, 0)";
+}
+
+
+function scrollHandler(e)
+{
+	scrollVal = Math.max(window.pageYOffset,0);
+	
+	/* UNCOMMENT THESE LINES TO ABORT PARALLAX STUFF */
+
+	// isTouchscreen = 1;
+	// top.setAttribute("class", "touch");
+	// body.style.backgroundImage = 'none';
+
+	
+	if ( isTouchscreen != 1 )
+	{
+		parallaxy()
+	}
+}
+
+body.onload = setup();
+document.addEventListener('scroll', scrollHandler, false);
